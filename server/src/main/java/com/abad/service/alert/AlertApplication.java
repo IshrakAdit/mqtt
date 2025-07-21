d1875d629533351c2209ec4ce441c225a4d6d9a3
IshrakAdit <ishrak.adit07@gmail.com>
2025-07-21 21:36:15 +0600

    setup: main file & Dockerfile

--- server/src/main/java/com/abad/service/alert/AlertApplication.java
+++ server/src/main/java/com/abad/service/alert/AlertApplication.java
@@ -1,13 +1,19 @@
 package com.abad.service.alert;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlertApplication {
+public class AlertApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlertApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("CareSync Data Service");
 	}
 
 }
